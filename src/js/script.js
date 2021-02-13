9765cf51b26e62dff75fdc8fd04ad637cd7b0af5
Igor Woźniak <igor91wozniak@gmail.com>
2021-02-13 15:28:14 +0100

    add RWD

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+{
+  function toggleMenu() {
+    console.log('ahgsggs');
+    const navButton = document.querySelector('.burger');
+    const sidenav = document.querySelector('.menu');
+
+    navButton.addEventListener('click', function(e){
+      e.preventDefault();
+
+      sidenav.classList.toggle('nav-active');
+      console.log('gfff');
+    });
+  }
+
+  const app = () => {
+    toggleMenu();
+  };
+
+  app();
+
+}
